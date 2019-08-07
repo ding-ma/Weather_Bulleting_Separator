ef9d440cfd79663842bb878c8e9e82bf7ed5eb26
Ding Ma <ding.ma@mail.mcgill.ca>
2019-08-07 08:59:35 -0400

    automatic retrieval works, running it everyday

--- src/DDretrieval.py
+++ src/DDretrieval.py
@@ -1,13 +1,20 @@
 import re
 import urllib.request
+from datetime import datetime, timedelta
 
-for i in range(1, 6):
-    url = "https://dd.weather.gc.ca/bulletins/alphanumeric/2019080" + str(i) + "/FL/CWUL/19/"
+today = datetime.today()
+yesterday = (today - timedelta(days=1)).strftime("%Y%m%d")
+hourslst = ["08", "19"]
+for h in hourslst:
+    url = "https://dd.weather.gc.ca/bulletins/alphanumeric/" + yesterday + "/FL/CWUL/" + h + "/"
     req = urllib.request.Request(url)
     response = urllib.request.urlopen(req)
     the_page = response.read()
     page = str(the_page)
     flcn41 = re.compile(">FLCN41_CWUL")
-    flcnURl = re.compile("FLCN41_CWUL_.*<")
     for bulletin in flcn41.finditer(page):
-        print(page[bulletin.start() + 1:bulletin.end() + 15])
+        pageULR = url + page[bulletin.start() + 1:bulletin.end() + 15]
+        req1 = urllib.request.Request(pageULR)
+        response1 = urllib.request.urlopen(req1)
+        the_page1 = response1.read()
+        urllib.request.urlretrieve(pageULR, "downloads/CWUL_FLCN41_" + yesterday + "_h_" + h + ".txt")
