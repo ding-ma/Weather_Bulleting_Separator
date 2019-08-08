3b1c8911c6878b874f1eaf4542c96656235942eb
Ding Ma <ding.ma@mail.mcgill.ca>
2019-08-08 10:12:43 -0400

    fixed how emails are sent

--- src/DDretrieval.py
+++ src/DDretrieval.py
@@ -1,6 +1,8 @@
 import os
 import re
 import shutil
+import smtplib
+import urllib.error
 import urllib.request
 from datetime import datetime, timedelta
 
@@ -11,6 +13,15 @@ if os.path.exists(path + "tempdir"):
 else:
     os.mkdir(path + "tempdir")
 
+
+def sendemail(errormsg):
+    sender = 'InfoSmog-FLCN41-Process'
+    receivers = ['ding.ma@canada.ca']
+
+    smtpObj = smtplib.SMTP('localhost')
+    smtpObj.sendmail(sender, receivers, errormsg)
+
+
 today = datetime.today()
 yesterday = (today - timedelta(days=1)).strftime("%Y%m%d")
 url = "https://dd.weather.gc.ca/bulletins/alphanumeric/" + yesterday + "/FL/CWUL/"
@@ -19,14 +30,27 @@ resp = urllib.request.urlopen(request)
 hourpage = str(resp.read())
 findalldigits = re.compile(">\d{2}")
 flcn41 = re.compile(">FLCN41_CWUL_")
+hlst = []
 for hourdigit in findalldigits.finditer(hourpage):
     urlwithhour = url + hourdigit.group()[1:]
     req = urllib.request.Request(urlwithhour)
     response = urllib.request.urlopen(req)
     bulletinpage = str(response.read())
     for bulletin in flcn41.finditer(bulletinpage):
+        hlst.append(bulletin)
         download = bulletinpage[bulletin.start() + 1:bulletin.end() + 22].split("<")
         downloadurl = urlwithhour + "/" + download[0]
-        urllib.request.urlretrieve(downloadurl, "CWUL_FLCN41_" + yesterday + "_h_" + hourdigit.group()[1:] + ".txt")
+        try:
+            urllib.request.urlretrieve(downloadurl,
+                                       path + "tempdir/CWUL_FLCN41_" + yesterday + "_h_" + hourdigit.group()[
+                                                                                           1:] + ".txt")
+            urllib.request.urlretrieve(downloadurl,
+                                       path + "datamart_downloads/CWUL_FLCN41_" + yesterday + "_h_" + hourdigit.group()[
+                                                                                                      1:] + ".txt")
+        except urllib.error.HTTPError:
+            sendemail("Error occured while downloading FLCN41-CWUL for hour: " + hourdigit.group()[1:])
 
-os.system("cd /home/ && /usr/bin/python3 /home/sair001/public_html/bulletin_infoSmog/script/infoSmog-DailyScript.py")
+if len(hlst) < 2:
+    sendemail("Less than 2 bulletins was downloaded for " + yesterday)
+
+os.system("cd /home/ && /usr/bin/python3 /home/sair001/public_html/bulletin_infoSmog/script/infoSmogDailyScript.py")
